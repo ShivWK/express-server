342d649a7ba2af50f2e8633a2b0dff7cc889e01b
Admin <shivendrawk@gmail.com>
2025-05-16 13:12:44 +0530

    Add environment variables

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const PORT = 3000;
+console.log(app.get("env"));
+console.log(process.env);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("server has started...");
 });
