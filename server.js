ff07e8ab185e1f1d29d0ac071975015bc8980e7c
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-05-22 12:41:26 +0530

    Add remote database amd mongoose

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+mongoose.connect(process.env.REMOTE_CONN_STR, {
+  // useNewUrlParser: true,
+}).then(conn => {
+  console.log("Connection Successful");
+  // console.log(conn);
+}).catch(err => {
+  console.log("Error occured while connecting");
+  console.log(err);
+})
 
 // console.log(app.get("env"));
 // console.log(process.env);
