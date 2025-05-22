fb17615d4e3edc09448114e6d0662eca0c616908
Shivendra Dwivedi <shivendrawk@gmail.com>
2025-05-22 12:53:35 +0530

    Add schema and modal

--- server.js
+++ server.js
@@ -13,6 +13,33 @@ mongoose.connect(process.env.REMOTE_CONN_STR, {
   console.log(err);
 })
 
+const movieSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required field!"],
+    unique: true,
+  },
+  description: String,
+  duration:  {
+    type: Number,
+    required: [true, "Duration is required field!"]
+  },
+  rating:  {
+    type: String,
+    default: 1.0,
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+const indianMovie= new Movie({
+  name: "INDIA meri jaan",
+  description: "A movie dedicated to the matayred soldirs of INDIA",
+  duration: 250,
+  rating: 5,
+})
+
+
 // console.log(app.get("env"));
 // console.log(process.env);
 
